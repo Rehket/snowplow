b73cfd5893f658e5da2d43a638b5a443fcd05165
Rehket <aalbright425@gmail.com>
2022-02-14 22:28:35 -0500

    Adding retries around salesforce api calls

--- ReadMe.md
+++ ReadMe.md
@@ -1,2 +1,2 @@
 # SnowPlow - A tool for doing stuff between SalesForce and Snowflake.
-## Pre-Release - Do not expect much before this gets to version 1.0
+## Pre-Release - Do not expect much before this gets to version 1.0.0
--- snowplow/sfdc.py
+++ snowplow/sfdc.py
@@ -1,11 +1,13 @@
 import os
-import sys
-import snowflake.connector
 from snowplow.auth import get_salesforce_client
 from snowplow.controller import TableObjectField, TableObject
-from pydantic import parse_obj_as
-from typing import List
 import httpx
+from tenacity import (
+    Retrying,
+    stop_after_attempt,
+    wait_exponential,
+    retry_if_exception_type,
+)
 
 
 def get_object_definition(
@@ -14,20 +16,42 @@ def get_object_definition(
     api_version: str = "53.0",
     skip_fields: str = os.environ.get("SFDC_SKIP_FIELDS"),
     include_compound_fields: bool = False,
+    max_attempts: int = os.getenv("SFDC_MAX_DOWNLOAD_ATTEMPTS", 20),
 ) -> TableObject:
+    """
+    Get SObject Metadata from SFDC to compare against SnowFlake.
+    :param object: The Api name of the object to retrieve.
+    :param client: A client authenticated against salesforce
+    :param api_version: The api version to use when talking to salesforce.
+    :param skip_fields: Fields to skip when building the object definition.
+    :param include_compound_fields: Whether or not to include compound fields in the object definition.
+    :param max_attempts: The maximum number of attempts before gining up.
+    :return: The table object.
+    """
     if skip_fields:
         skip_fields = [field.lower() for field in skip_fields.split(",")]
     else:
         skip_fields = []
 
-    response = client.get(
-        url=f"/services/data/v{api_version}/sobjects/{object}/describe/"
-    )
+    try:
+        for attempt in Retrying(
+            retry=retry_if_exception_type(httpx.ReadTimeout),
+            stop=stop_after_attempt(max_attempts),
+            wait=wait_exponential(multiplier=1, min=4, max=60),
+        ):
+            with attempt:
+                response = client.get(
+                    url=f"/services/data/v{api_version}/sobjects/{object}/describe/"
+                )
+    finally:
+        client.close()
     object_data = response.json()
     if include_compound_fields:
         compound_field_names = []
     else:
-        compound_field_names = [field.get("compoundFieldName") for field in object_data.get("fields")]
+        compound_field_names = [
+            field.get("compoundFieldName") for field in object_data.get("fields")
+        ]
 
     table = TableObject(
         name=object_data.get("name"),
@@ -35,8 +59,10 @@ def get_object_definition(
         fields=[
             TableObjectField(**field)
             for field in response.json().get("fields")
-            if (field.get("name") not in skip_fields and field.get("name") not in compound_field_names)
-
+            if (
+                field.get("name") not in skip_fields
+                and field.get("name") not in compound_field_names
+            )
         ],
         system="salesforce",
     )
